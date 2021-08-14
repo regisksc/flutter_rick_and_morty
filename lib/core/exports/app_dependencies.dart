ddd7c147034066fb377bbb7490a3af2c24ec6724
Regis Kian <regisksc@gmail.com>
2021-08-14 13:13:27 -0300

    added some needed dependencies

---
+++ lib/core/exports/app_dependencies.dart
@@ -0,0 +1,3 @@
+export 'package:dartz/dartz.dart';
+export 'package:equatable/equatable.dart';
+export 'package:http/http.dart';
---
+++ lib/core/exports/test_dependencies.dart
@@ -0,0 +1,3 @@
+export 'package:flutter_test/flutter_test.dart';
+export 'package:mocktail/mocktail.dart';
+export 'package:faker/faker.dart';
