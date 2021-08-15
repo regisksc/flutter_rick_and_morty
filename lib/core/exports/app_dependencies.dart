ed6726c03039375266885d186328ea54cca3d8da
Regis Kian <regisksc@gmail.com>
2021-08-15 02:59:04 -0300

    added exports

--- lib/core/exports/app_dependencies.dart
+++ lib/core/exports/app_dependencies.dart
@@ -1,3 +1,3 @@
-export 'package:dartz/dartz.dart';
+export 'package:dartz/dartz.dart' hide Evaluation;
 export 'package:equatable/equatable.dart';
-export 'package:http/http.dart';
+export 'package:dio/dio.dart';
