147b1798191078e20c49f00708c002d7de89235d
Regis Kian <regisksc@gmail.com>
2021-08-15 02:58:52 -0300

    changed language

--- lib/core/domain/errors/failures/no_connection_failure.dart
+++ lib/core/domain/errors/failures/no_connection_failure.dart
@@ -3,7 +3,7 @@ import 'failures.dart';
 class NoConnectionFailure extends BaseFailure {
   const NoConnectionFailure()
       : super(
-          title: 'Falha de conexão',
-          message: 'Ops, algo deu errado. Tente novamente daqui uns minutos',
+          title: 'Connection Failure',
+          message: "It seems you don't have connection right now",
         );
 }
--- lib/core/domain/errors/failures/unrecognized_failure.dart
+++ lib/core/domain/errors/failures/unrecognized_failure.dart
@@ -3,7 +3,7 @@ import 'failures.dart';
 class UnrecognizedFailure extends BaseFailure {
   const UnrecognizedFailure()
       : super(
-          title: 'Erro desconhecido',
-          message: 'Ops, algo deu errado. Tente novamente daqui uns minutos',
+          title: 'Unrecognized Error',
+          message: 'Oops, something happenned. Try again in a few minutes',
         );
 }
